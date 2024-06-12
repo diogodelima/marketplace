bcb9d0811cf27b92c35be244cb8ca485b91cc77f
Diogo Lima <diogocerqueiradeelima@gmail.com>
2024-06-12 15:15:56 +0100

    Initial commit

---
+++ src/test/kotlin/pt/diogo/marketplace/MarketplaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.diogo.marketplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
